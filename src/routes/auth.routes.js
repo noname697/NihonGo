9de63488e027239b5ed37b8a0117b0677e4f87f4
Arthur Manenti <arthur.manenti.videira@gmail.com>
2026-06-20 19:53:33 -0300

    feat: Criando rota para 'me' e aplicando o middleware

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const authMiddleware = require("../middlewares/auth.middleware");
 const authController = require("../controllers/auth.controller");
 
 const router = express.Router();
 
 router.post("/register", authController.register);
 router.post("/login", authController.login);
+router.get("/me", authMiddleware, authController.me);
 
 module.exports = router;
