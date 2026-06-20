e21fbd1a5d8d16779ae847d85f102ae44b333109
Arthur Manenti <arthur.manenti.videira@gmail.com>
2026-06-20 17:44:39 -0300

    feat: Criando model e migration de User

---
+++ models/user.js
@@ -0,0 +1,35 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    static associate(models) {}
+  }
+  User.init(
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password_hash: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      role: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaultValue: "student",
+      },
+    },
+    {
+      sequelize,
+      modelName: "User",
+      tableName: "users",
+    },
+  );
+  return User;
+};
