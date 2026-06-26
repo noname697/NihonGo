d0bc4cd88bedc07ae4e643744882bc4fcb34e291
Arthur Manenti <arthur.manenti.videira@gmail.com>
2026-06-26 19:06:41 -0300

    feat: Adaptando migrations de caracteres

--- migrations/20260626215138-create-study-character.js
+++ migrations/20260626215138-create-study-character.js
@@ -1,55 +1,70 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('StudyCharacters', {
+    await queryInterface.createTable("study_characters", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       type: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
       },
       symbol: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
       },
       romaji: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: true,
       },
       meaning: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: true,
       },
       onyomi: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: true,
       },
       kunyomi: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: true,
       },
       jlpt_level: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: true,
       },
       stroke_count: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: true,
       },
       character_group: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: true,
       },
       position: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
+
+    await queryInterface.addIndex("study_characters", ["type"]);
+    await queryInterface.addIndex("study_characters", ["jlpt_level"]);
+    await queryInterface.addIndex("study_characters", ["type", "position"]);
+    await queryInterface.addIndex("study_characters", ["type", "symbol"]);
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('StudyCharacters');
-  }
+    await queryInterface.dropTable("study_characters");
+  },
 };
--- migrations/20260626215351-create-user-character-progress.js
+++ migrations/20260626215351-create-user-character-progress.js
@@ -1,49 +1,75 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('UserCharacterProgresses', {
+    await queryInterface.createTable("user_character_progress", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       user_id: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "users",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
       },
       character_id: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "study_characters",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
       },
       correct_attempts: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
       },
       wrong_attemps: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
       },
       last_awnser: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: true,
       },
       last_result: {
-        type: Sequelize.BOOLEAN
+        type: Sequelize.BOOLEAN,
+        allowNull: true,
       },
       mastery_score: {
-        type: Sequelize.FLOAT
+        type: Sequelize.FLOAT,
+        allowNull: false,
+        defaultValue: 0,
       },
       last_practiced_at: {
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        allowNull: true,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
+
+    await queryInterface.addIndex("user_character_progress", ["user_id"]);
+    await queryInterface.addIndex("user_character_progress", ["character_id"]);
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('UserCharacterProgresses');
-  }
+    await queryInterface.dropTable("user_character_progress");
+  },
 };
