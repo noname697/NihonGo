ff139e6268d7d29108d2e102d25309ef70f535a9
Arthur Manenti <arthur.manenti.videira@gmail.com>
2026-07-01 17:37:39 -0300

    feat: Criando fetchs de autenticação

---
+++ frontend/src/api/auth.api.js
@@ -0,0 +1,16 @@
+import api from "./api";
+
+export const registerUser = async (data) => {
+  const response = await api.post("/api/auth/register", data);
+  return response.data;
+};
+
+export const loginUser = async (data) => {
+  const response = await api.post("/api/auth/login", data);
+  return response.data;
+};
+
+export const getMe = async (data) => {
+  const response = await api.get("/api/auth/me", data);
+  return response.data;
+};
