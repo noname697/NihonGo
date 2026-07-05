7d83bb151b646f16b746de423c3cb3f0d0c0afcb
Arthur Manenti <arthur.manenti.videira@gmail.com>
2026-07-04 21:16:33 -0300

    feat(backend): Criando primeiro Screen Service

---
+++ backend/src/services/screens/lesson.service.js
@@ -0,0 +1,16 @@
+const contentService = require("../content.service");
+
+const getLessonScreen = async (lessonId) => {
+  const lesson = await contentService.getLessonById(lessonId);
+
+  const { exercises } = await contentService.getLessonExercises(lessonId);
+
+  return {
+    lesson: {
+      ...lesson.toJSON(),
+      exercises,
+    },
+  };
+};
+
+module.exports = { getLessonScreen };
