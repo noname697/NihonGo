b900cf0174edd89a96737a8d2df16b1c8ef9b9d0
Arthur Manenti <arthur.manenti.videira@gmail.com>
2026-07-08 18:11:30 -0300

    feat: Criando flashcard API

---
+++ frontend/src/api/flashcard.api.js
@@ -0,0 +1,28 @@
+import api from "./api";
+
+export const getDecks = async () => {
+  const response = await api.get("/api/flashcards/decks");
+  return response.data;
+};
+
+export const createDeck = async (payload) => {
+  const response = await api.post("/api/flashcards/decks", payload);
+  return response.data;
+};
+
+export const getDueCards = async (params = {}) => {
+  const response = await api.get("/api/flashcards/due", { params });
+  return response.data;
+};
+
+export const reviewCard = async (id, isCorrect) => {
+  const response = await api.post(`/api/flashcards/cards/${id}/review`, {
+    is_correct: isCorrect,
+  });
+  return response.data;
+};
+
+export const getFlashcardProgress = async () => {
+  const response = await api.get("/api/flashcards/progress");
+  return response.data;
+};
